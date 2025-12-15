3ea141558518e26b2d3bf644195ed63f522728ae
azufb <curiousazunas@gmail.com>
2025-12-15 20:41:18 +0900

    HistoireでStory作成

---
+++ src/stories/histoire/Button.stories.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import Button from '@/components/Button.vue';
+    import { BUTTON_VARIANTS } from '@/const/buttonVariants.js';
+</script>
+
+<template>
+    <Story title="Button">
+        <Variant title="success">
+            <Button label="Success Button" :variant="BUTTON_VARIANTS.SUCCESS" :onClick="() => alert('Success button clicked')" />
+        </Variant>
+        
+        <Variant title="danger">
+            <Button label="Danger Button" :variant="BUTTON_VARIANTS.DANGER" :onClick="() => alert('Danger button clicked')" />
+        </Variant>
+
+        <Variant title="default">
+            <Button label="Default Button" :variant="BUTTON_VARIANTS.DEFAULT" :onClick="() => alert('Default button clicked')" />
+        </Variant>
+    </Story>
+</template>
---
+++ src/stories/histoire/RadioButton.stories.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import RadioButton from '@/components/RadioButton.vue';
+</script>
+
+<template>
+    <Story title="RadioButton">
+        <Variant title="default">
+            <RadioButton label="Option 1" value="option1" />
+            <RadioButton label="Option 2" value="option2" />
+            <RadioButton label="Option 3" value="option3" />
+        </Variant>
+    </Story>
+</template>
